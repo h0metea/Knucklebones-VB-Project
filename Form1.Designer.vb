8f75e1feda316bb8f3ca6b0dd752c29914c444b7
hometea <hometea@proton.me>
2025-12-03 16:24:47 -0500

    please actually commit :DDD

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,6 +27,19 @@ Partial Class Form1
         GameTimer = New Timer(components)
         btnDbgStart = New Button()
         btnDbgStop = New Button()
+        btnP1_1x1 = New Button()
+        btnP1_1x2 = New Button()
+        btnP1_1x3 = New Button()
+        btnP1_2x3 = New Button()
+        btnP1_2x2 = New Button()
+        btnP1_2x1 = New Button()
+        btnP1_3x1 = New Button()
+        btnP1_3x2 = New Button()
+        btnP1_3x3 = New Button()
+        btnTMPDICE = New Button()
+        lblDiceTemp = New Label()
+        pboxDice = New PictureBox()
+        CType(pboxDice, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' lblTime
@@ -61,16 +74,136 @@ Partial Class Form1
         btnDbgStop.Text = "Stop / Skip"
         btnDbgStop.UseVisualStyleBackColor = True
         ' 
+        ' btnP1_1x1
+        ' 
+        btnP1_1x1.FlatStyle = FlatStyle.Flat
+        btnP1_1x1.Location = New Point(60, 152)
+        btnP1_1x1.Name = "btnP1_1x1"
+        btnP1_1x1.Size = New Size(64, 63)
+        btnP1_1x1.TabIndex = 3
+        btnP1_1x1.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_1x2
+        ' 
+        btnP1_1x2.FlatStyle = FlatStyle.Flat
+        btnP1_1x2.Location = New Point(130, 152)
+        btnP1_1x2.Name = "btnP1_1x2"
+        btnP1_1x2.Size = New Size(64, 63)
+        btnP1_1x2.TabIndex = 3
+        btnP1_1x2.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_1x3
+        ' 
+        btnP1_1x3.FlatStyle = FlatStyle.Flat
+        btnP1_1x3.Location = New Point(200, 152)
+        btnP1_1x3.Name = "btnP1_1x3"
+        btnP1_1x3.Size = New Size(64, 63)
+        btnP1_1x3.TabIndex = 3
+        btnP1_1x3.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_2x3
+        ' 
+        btnP1_2x3.FlatStyle = FlatStyle.Flat
+        btnP1_2x3.Location = New Point(200, 221)
+        btnP1_2x3.Name = "btnP1_2x3"
+        btnP1_2x3.Size = New Size(64, 63)
+        btnP1_2x3.TabIndex = 3
+        btnP1_2x3.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_2x2
+        ' 
+        btnP1_2x2.FlatStyle = FlatStyle.Flat
+        btnP1_2x2.Location = New Point(130, 221)
+        btnP1_2x2.Name = "btnP1_2x2"
+        btnP1_2x2.Size = New Size(64, 63)
+        btnP1_2x2.TabIndex = 3
+        btnP1_2x2.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_2x1
+        ' 
+        btnP1_2x1.FlatStyle = FlatStyle.Flat
+        btnP1_2x1.Location = New Point(60, 221)
+        btnP1_2x1.Name = "btnP1_2x1"
+        btnP1_2x1.Size = New Size(64, 63)
+        btnP1_2x1.TabIndex = 3
+        btnP1_2x1.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_3x1
+        ' 
+        btnP1_3x1.FlatStyle = FlatStyle.Flat
+        btnP1_3x1.Location = New Point(60, 290)
+        btnP1_3x1.Name = "btnP1_3x1"
+        btnP1_3x1.Size = New Size(64, 63)
+        btnP1_3x1.TabIndex = 3
+        btnP1_3x1.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_3x2
+        ' 
+        btnP1_3x2.FlatStyle = FlatStyle.Flat
+        btnP1_3x2.Location = New Point(130, 290)
+        btnP1_3x2.Name = "btnP1_3x2"
+        btnP1_3x2.Size = New Size(64, 63)
+        btnP1_3x2.TabIndex = 3
+        btnP1_3x2.UseVisualStyleBackColor = True
+        ' 
+        ' btnP1_3x3
+        ' 
+        btnP1_3x3.FlatStyle = FlatStyle.Flat
+        btnP1_3x3.Location = New Point(200, 290)
+        btnP1_3x3.Name = "btnP1_3x3"
+        btnP1_3x3.Size = New Size(64, 63)
+        btnP1_3x3.TabIndex = 3
+        btnP1_3x3.UseVisualStyleBackColor = True
+        ' 
+        ' btnTMPDICE
+        ' 
+        btnTMPDICE.Location = New Point(642, 183)
+        btnTMPDICE.Name = "btnTMPDICE"
+        btnTMPDICE.Size = New Size(75, 23)
+        btnTMPDICE.TabIndex = 4
+        btnTMPDICE.Text = "dice test"
+        btnTMPDICE.UseVisualStyleBackColor = True
+        ' 
+        ' lblDiceTemp
+        ' 
+        lblDiceTemp.AutoSize = True
+        lblDiceTemp.Location = New Point(577, 187)
+        lblDiceTemp.Name = "lblDiceTemp"
+        lblDiceTemp.Size = New Size(41, 15)
+        lblDiceTemp.TabIndex = 5
+        lblDiceTemp.Text = "Label1"
+        ' 
+        ' pboxDice
+        ' 
+        pboxDice.Location = New Point(450, 166)
+        pboxDice.Name = "pboxDice"
+        pboxDice.Size = New Size(77, 81)
+        pboxDice.TabIndex = 6
+        pboxDice.TabStop = False
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(pboxDice)
+        Controls.Add(lblDiceTemp)
+        Controls.Add(btnTMPDICE)
+        Controls.Add(btnP1_3x3)
+        Controls.Add(btnP1_3x2)
+        Controls.Add(btnP1_3x1)
+        Controls.Add(btnP1_2x1)
+        Controls.Add(btnP1_2x2)
+        Controls.Add(btnP1_2x3)
+        Controls.Add(btnP1_1x3)
+        Controls.Add(btnP1_1x2)
+        Controls.Add(btnP1_1x1)
         Controls.Add(btnDbgStop)
         Controls.Add(btnDbgStart)
         Controls.Add(lblTime)
         Name = "Form1"
         Text = "Form1"
+        CType(pboxDice, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -79,5 +212,17 @@ Partial Class Form1
     Friend WithEvents GameTimer As Timer
     Friend WithEvents btnDbgStart As Button
     Friend WithEvents btnDbgStop As Button
+    Friend WithEvents btnP1_1x1 As Button
+    Friend WithEvents btnP1_1x2 As Button
+    Friend WithEvents btnP1_1x3 As Button
+    Friend WithEvents btnP1_2x3 As Button
+    Friend WithEvents btnP1_2x2 As Button
+    Friend WithEvents btnP1_2x1 As Button
+    Friend WithEvents btnP1_3x1 As Button
+    Friend WithEvents btnP1_3x2 As Button
+    Friend WithEvents btnP1_3x3 As Button
+    Friend WithEvents btnTMPDICE As Button
+    Friend WithEvents lblDiceTemp As Label
+    Friend WithEvents pboxDice As PictureBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,15 +1,53 @@
-﻿Public Class Form1
-    Dim mgametimer As Double, PlaceholderEnd As Integer
+﻿Imports Knucklebones_Port_Project.My.Resources
 
+Public Class Form1
+    Dim Dice As Integer
+
+    Public Function Rand() ' dice rolling fuck yea!!! also this might be how 2 write a function LOL
+        Dice = Int((6 - 1 + 1) * Rnd() + 1)
+    End Function
+
+    Private P1intButtonID As Integer = 0
+
+
+    Private Sub Reset()
+        For Each ctl In Controls
+            If TypeOf ctl Is Button Then
+                ctl.BackgroundImage
+                ctl.Enabled = True
+            End If
+        Next ctl
+        btnDbgStart.Text = "Start / Restart"
+    End Sub
+
+    ' ok below is where all the double clicking happens
     Private Sub btnDbgStart_Click(sender As Object, e As EventArgs) Handles btnDbgStart.Click
-        lblTime.Caption = "10"
-        GameTimer.Interval = 1000
-        mgametimer = Now
+        ' move this to the starter screen after its made
+        Reset()
+
+        btnP1_1x1
+        P1intButtonID = 1
+        GameTimer.Start()
+
+
     End Sub
 
     Private Sub GameTimer_Tick(sender As Object, e As EventArgs) Handles GameTimer.Tick
 
-        PlaceholderEnd
+
 
     End Sub
+
+    Private Sub btnTMPDICE_Click(sender As Object, e As EventArgs) Handles btnTMPDICE.Click
+        Rand()
+        lblDiceTemp.Text = Dice
+        pboxDice.Image = Resources.Dice1
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' oops misclick, does this do smth when the form opens?
+        ' maybe could wrap this entire shit around it xDDD
+    End Sub
+
 End Class
